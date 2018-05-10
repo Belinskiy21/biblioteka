b9b0596cd97190ee3b68f24a118a59c888154c44
Oleg <belinskiy21@gmail.com>
2018-05-10 22:59:29 +0200

    add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,8 @@
 class Book < ApplicationRecord
   mount_uploader :file, ImageUploader
+  has_many :history_cards
+
+  validates :title, :author, :published_at, :description, presence: true
 
   def self.return_book(book_id)
     book = Book.find_by(id: book_id)

39f288edb7098ee8ca0d7000a7b0c66449568641
Oleg <belinskiy21@gmail.com>
2018-05-13 09:21:41 +0300

    add througth to models

--- app/models/history_card.rb
+++ app/models/history_card.rb
@@ -1,6 +1,9 @@
 class HistoryCard < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :user_id, :book_id, :book_title, presence: true
+
   def self.history_saver(user_id, book_id, book_title, book_reader)
     card = HistoryCard.new(user_id: user_id, book_id: book_id, book_title: book_title, book_reader: book_reader)
     card.save
--- spec/models/history_card_spec.rb
+++ spec/models/history_card_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 RSpec.describe HistoryCard, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:book) }
+  it { should validate_presence_of(:book_title) }
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:book_id) }
 end
